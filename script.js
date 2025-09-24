0444b4c589b07ef7707140890244e334837d3cf9
BlockwellConsulting <83551748+BlockwellConsulting@users.noreply.github.com>
2025-09-24 12:01:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-L.geoJSON({
-  "type": "FeatureCollection",
-  "features": [
-    { "type": "Feature",
-      "properties": {"name": "Test Polling Place"},
-      "geometry": {"type": "Point", "coordinates": [-81.6326, 38.3498]}
+mapboxgl.accessToken = 'YOUR_MAPBOX_TOKEN';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-81.6326, 38.3498],
+  zoom: 5
+});
+
+map.on('load', function () {
+  map.addSource('pollingPlaces', {
+    type: 'vector',
+    url: 'mapbox://yourusername.polling_places'
+  });
+
+  map.addLayer({
+    id: 'pollingPoints',
+    type: 'circle',
+    source: 'pollingPlaces',
+    'source-layer': 'polling_places',
+    paint: {
+      'circle-radius': 3,
+      'circle-color': '#e31a1c'
     }
-  ]
-}).addTo(map);
+  });
+});
